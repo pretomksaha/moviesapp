3bd4ea84678bb4367169b2f3454ad4bc38d1c813
pretomksaha <pretomksaha@yahoo.com>
2022-04-08 23:38:13 +0200

    add gitignore

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const bodyParser= require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+const PORT = 4000; // backend routing port.
+
+app.listen(PORT, ()=> {
+    console.log('Server is running on port ${PORT}.');
+});
---
+++ backend/routes/handler.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/home',(req,res) => {
+    const str = [{
+        "name": "PS",
+        "email": "pretomksaha@yahoo.com"
+    }];
+});
