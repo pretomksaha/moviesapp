a6de2d4784ef48c367f14048732a91babe7bc524
pretomksaha <pretomksaha@yahoo.com>
2022-04-09 21:15:24 +0200

    create and connect database

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const bodyParser= require('body-parser');
 const routerHandler = require('./routes/handler.js');
+const mongoose = require('mongoose');
+require('dotenv/config');
+
 
 const app = express();
 
@@ -8,7 +11,16 @@ app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 app.use('/', routerHandler)
 
-const PORT = 4000; // backend routing port.
+// DB connection
+mongoose.connect(process.env.DB_URI, {useNewUrlParser:true, useUnifiedTopology:true})
+.then( () => {
+    console.log('DB Connected!')
+})
+.catch((err) => {
+    console.log(err);
+});
+
+const PORT = process.env.PORT || 4000; // backend routing port.
 
 app.listen(PORT, ()=> {
     console.log('Server is running on port',PORT);
