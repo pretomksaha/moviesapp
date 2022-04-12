859330b0652ed6bbe038b9f2a5a3f60ba2dea31d
pretomksaha <pretomksaha@yahoo.com>
2022-04-12 20:02:34 +0200

    handler test case

--- backend/tests/handler.test.js
+++ backend/tests/handler.test.js
@@ -3,11 +3,10 @@ const handler = require("../routes/handler");
 
 
 
-describe("Get /find", () => {
-
+describe("Get /search", () => {
     describe("when passed a SearchItem", () => {
       test("should respond with a 200 status code",  async () => {
-        const res = await request(handler).get("/search?:").query({ SearchItem: 'space' });
+        const res = await request(handler).get("/search").query({ SearchItem: 'space' });
         expect(res.statusCode).toEqual(200);
       });
     })
